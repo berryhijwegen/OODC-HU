027afe3f8630a127a4f7317c91c5c30865c2cf86
Berry Hijwegen <berry.hijwegen@student.hu.nl>
2019-02-05 20:40:04 +0100

    added opdracht 2a

--- src/week1/les1/opdracht5/Student.java
+++ src/week1/les1/opdracht5/Student.java
@@ -4,28 +4,28 @@ public class Student{
 	private String naam;
 	private int studentNummer;
 
-public Student(String nm, int num){
-	naam = nm;
-	studentNummer = num;
+	public Student(String nm, int num){
+		naam = nm;
+		studentNummer = num;
 	}
 
-public Student(String nm) {
-	naam = nm;
-}
+	public Student(String nm) {
+		naam = nm;
+	}
 
-public String getNaam() {
-	return naam;
-}
+	public String getNaam() {
+		return naam;
+	}
 
-public int getStudentNummer() {
-	return studentNummer;
-}
+	public int getStudentNummer() {
+		return studentNummer;
+	}
 
-public void setStudentNummer(int newNum) {
-	studentNummer = newNum;
-}
+	public void setStudentNummer(int newNum) {
+		studentNummer = newNum;
+	}
 
-public String toString() {
-	return "naam van student is " + naam + ", studentnummer van " + naam + " is " + studentNummer + ".";
-}
+	public String toString() {
+		return "naam van student is " + naam + ", studentnummer van " + naam + " is " + studentNummer + ".";
+	}
 }

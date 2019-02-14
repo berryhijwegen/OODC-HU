0113c5959c390585d8c521fe2bed348e47758710
Berry Hijwegen <berry.hijwegen@student.hu.nl>
2019-02-14 09:50:45 +0100

    Changed 3b

--- src/inleveropdrachten/opdracht3b/AutoHuur.java
+++ src/inleveropdrachten/opdracht3b/AutoHuur.java
@@ -5,40 +5,56 @@ public class AutoHuur {
     private Auto gehuurdeAuto;
     private int aantalDagen;
 
-    public AutoHuur(Klant hrdr, Auto ghrdauto, int antldgn){
+    public AutoHuur(Klant hrdr, Auto ghrdauto, int antldgn) {
         huurder = hrdr;
         gehuurdeAuto = ghrdauto;
         aantalDagen = antldgn;
     }
 
-    public AutoHuur(){
+    public AutoHuur() {
 
     }
 
-    public void setHuurder(Klant hrdr){
+    public void setHuurder(Klant hrdr) {
         huurder = hrdr;
     }
 
-    public void setGehuurdeAuto(Auto ghrdauto){
+    public void setGehuurdeAuto(Auto ghrdauto) {
         gehuurdeAuto = ghrdauto;
     }
-    public Auto getGehuurdeAuto(){
+
+    public Auto getGehuurdeAuto() {
         return gehuurdeAuto;
     }
 
-    public void setAantalDagen(int antldgn){
+    public void setAantalDagen(int antldgn) {
         aantalDagen = antldgn;
     }
 
-    public double totaleKosten(Auto ghrdauto, Klant hrdr){
-        return (aantalDagen * ghrdauto.GetPrijs()) * ((100-hrdr.getKorting()) * 0.01);
+    public double totaleKosten(Auto ghrdauto, Klant hrdr) {
+        return (aantalDagen * ghrdauto.GetPrijs()) * ((100 - hrdr.getKorting()) * 0.01);
     }
 
-    public String toString(){
-        if (huurder == null){
-            return "  er is geen auto bekend\n  er is geen huurder bekend\n  aantal dagen: 0 en dat kost 0.0";
-        }
-        else {
+    public String toString() {
+        if (gehuurdeAuto == null || huurder == null || aantalDagen == 0) {
+            String s = "";
+            if (gehuurdeAuto == null) {
+                s += "  er is geen auto bekend\n  ";
+            } else {
+                s += String.format("  autotype: %s\n  ", gehuurdeAuto);
+            }
+            if (huurder == null) {
+                s += "er is geen huurder bekend\n  ";
+            } else {
+                s += String.format("op naam van: %s\n  ", huurder);
+            }
+            if (aantalDagen == 0) {
+                s += "aantal dagen: 0 en dat kost 0.0\n";
+            } else {
+                s += String.format("aantal dagen: %d en dat kost €%.2f", aantalDagen, totaleKosten(gehuurdeAuto, huurder));
+            }
+            return s;
+        } else {
             return String.format("  autotype: %s\n  op naam van: %s\n  aantal dagen: %d en dat kost €%.2f", gehuurdeAuto, huurder, aantalDagen, totaleKosten(gehuurdeAuto, huurder));
         }
     }
